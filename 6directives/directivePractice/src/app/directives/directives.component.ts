1dc15e1ea146aaead25ed72c3686ee88ab821c7d
dheeraj reddy <reddydheeraj110@gmail.com>
2018-05-21 17:39:17 +0530

    structral directive ngFor

--- 6directives/directivePractice/src/app/directives/directives.component.ts
+++ 6directives/directivePractice/src/app/directives/directives.component.ts
@@ -1,27 +1,35 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-directives',
   templateUrl: './directives.component.html',
   styleUrls: ['./directives.component.css']
 })
-export class DirectivesComponent implements OnInit , OnChanges {
+export class DirectivesComponent implements OnInit  {
   // isWell:Boolean=false;
  //fullColor : string;
-textColor:string;
+  textColor:string;
+  mobiles: string [];
   constructor() {
-
-   }
+    
+   this.mobiles =[];
+    this.mobiles.push('samsung');
+    this.mobiles.push('Motorola');
+    this.mobiles.push('Rim');
+    this.mobiles.push('Apple');
+    this.mobiles.push('Microsoft');
+    this.mobiles.push('Lenovo');
+    this.mobiles.push('Dell');
+    this.mobiles.push('oppo');
+    this.mobiles.push('vivo');
+    this.mobiles.push('Nokia')
+    
+   
+  }
 
   ngOnInit() {
     //this.fullColor ="1px solid "+ this.textColor;
   }
-  onClickIp(){
- //   console.log('Hi');
-   // this.isWell = !this.isWell;
-  }
-  ngOnChanges(){
-   // this.fullColor = "1px solid " + this.textColor;
-   // console.log(`Full Color ${this.fullColor}`)
-  }
+
+
 }
